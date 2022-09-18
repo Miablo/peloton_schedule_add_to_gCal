f237f315a1cadf4c4e310e5daea49d30da39ae45
Mio Diaz <themio@themioshow.com>
2022-09-18 19:05:47 -0400

    cal.py created

--- get_schedule_v2.py
+++ get_schedule_v2.py
@@ -3,13 +3,6 @@ import re as r
 import peloton as Peloton
 import json
 
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
 import time
 import datetime as DateTime
 from datetime import date
@@ -151,75 +144,6 @@ def _get_ride_details(session, ride_id, headers):
  #        ],
  #      },
  #    }
-
-
-def _calendar_api_call(event_response_body):
-    """
-
-    """
-    #start of google API code
-    # If modifying these scopes, delete the file token.json.
-    SCOPES = ['https://www.googleapis.com/auth/calendar.events']
-    creds = None
-
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=8080)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    try:
-        service = build('calendar', 'v3', credentials=creds)
-
-        # # Call the Calendar API
-        # new_cal_event(service)
-        # now = DateTime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        # print('Getting the upcoming 10 events')
-
-        # events_result = service.events().list(calendarId='primary', timeMin=now,
-        #                                       maxResults=10, singleEvents=True,
-        #                                       orderBy='startTime').execute()
-        # events = events_result.get('items', [])
-
-        # if not events:
-        #     print('No upcoming events found.')
-        #     return
-
-        # # Prints the start and name of the next 10 events
-        # for event in events:
-        #     start = event['start'].get('dateTime', event['start'].get('date'))
-        #     print(start, event['summary'])
-
-    except HttpError as error:
-        print('An error occurred: %s' % error)
-
-### TODO
-## Research google api
-## determine how build event for calendar event
-## create function to pull duration of class and time of class to create end_time var
-## determine what part will go into the event name ++ summary 
-###
-## https://developers.google.com/calendar/api/v3/reference
-def _new_cal_event(service): 
-    """
-
-
-    """
-
-    event = service.events().insert(calendarId='primary', body=event).execute()
-
 
 def main():
 	'''
