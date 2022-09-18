d080ac02a35aa822c1954dda46cf83a6206d3e95
Mio Diaz <themio@themioshow.com>
2022-09-18 18:58:53 -0400

    Update get_schedule_v2.py

--- get_schedule_v2.py
+++ get_schedule_v2.py
@@ -20,11 +20,9 @@ Copyright 2022 Mio Diaz
 
 """
 
-global usr_info
-
 base_url = 'https://api.onepeloton.com'
 
-def _peloton_session(s):
+def _speloton_session(s):
 
 	'''
 	Start peloton api session using username & password
@@ -33,13 +31,13 @@ def _peloton_session(s):
 
     Parameters
     ----------
-	s    
-		session.request()
+	s
+        session.request()
 
 	Returns
     -------
     dict
-        user id, session id, parameter id
+        user id, session id, etc
 
 	'''
 	payload = {'username_or_email': USERNAME, 'password': PASSWORD} 
@@ -72,12 +70,6 @@ def _get_reservations(session, usr_id, headers):
 
 	'''
 	reservation_url = base_url + '/api/user/' + usr_id + '/reservations'
-	
-	## additional items to retrieve and store in this api call
- #  "scheduled_start_time": 1585305000,
- #  "start_time": 1585305000,
- #  "end_time": 1585306084,
- #  "pedaling_start_time": 1585
 	    
 	response = session.get(reservation_url, headers=headers)
 	reservations = response.json()
@@ -126,7 +118,6 @@ def _get_ride_details(session, ride_id, headers):
 		headers used for all calls asking for json
 
 	'''
-	# # /api/ride/{rideId}/details
 	ride_url = base_url + '/api/ride/' + ride_id + '/details'
 
 	response = session.get(ride_url, headers=headers)
@@ -138,8 +129,6 @@ def _get_ride_details(session, ride_id, headers):
 	print(rides['ride']['title']) # class title
 	print(rides['ride']['instructor']['name'])
 	print(rides['ride']['scheduled_start_time'])
-
-
 
 	# event = {
  #      'summary': rides['ride']['description'],
@@ -164,8 +153,7 @@ def _get_ride_details(session, ride_id, headers):
  #    }
 
 
-
-def calendar_api_call(event_response_body):
+def _calendar_api_call(event_response_body):
     """
 
     """
@@ -224,32 +212,11 @@ def calendar_api_call(event_response_body):
 ## determine what part will go into the event name ++ summary 
 ###
 ## https://developers.google.com/calendar/api/v3/reference
-def new_cal_event(service): 
+def _new_cal_event(service): 
     """
 
 
     """
-    event = {
-      'summary': summary,
-      'start': {
-        'dateTime': start_time,
-        'timeZone': 'GMT-03:00',
-      },
-      'end': {
-        'dateTime': end_time,
-        'timeZone': 'GMT-03:00',
-      },
-      'recurrence': [
-        'RRULE:FREQ=DAILY;COUNT=2'
-      ],
-      'reminders': {
-        'useDefault': False,
-        'overrides': [
-          {'method': 'email', 'minutes': 24 * 60},
-          {'method': 'popup', 'minutes': 10},
-        ],
-      },
-    }
 
     event = service.events().insert(calendarId='primary', body=event).execute()
 
@@ -262,9 +229,9 @@ def main():
 	'''
 	s = requests.Session()
 
-	usr_dict = _peloton_session(s)
+	usr_info = _peloton_session(s)
 
-	usr_id = usr_dict['user_id']
+	usr_id = usr_info['user_id']
 	headers = {
         "Content-Type": "application/json",
     }
