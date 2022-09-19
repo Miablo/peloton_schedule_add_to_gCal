d2425d0478da513e3bfe1abdeb6b0328087b2077
Mio Diaz <themio@themioshow.com>
2022-09-18 23:23:19 -0400

    testing emojis

--- get_schedule_v2.py
+++ get_schedule_v2.py
@@ -3,9 +3,11 @@ import peloton as Peloton
 import json
 
 import datetime as _date_time
-import google_cal_event_creator as _event_creator
+import google_cal_event_creator as event_creator
 from dotenv import load_dotenv
-import os 
+import os
+
+from enum import Enum as enum
 
 """
 Created on Wed Sep 7 08:45PM 2022
@@ -20,6 +22,13 @@ load_dotenv()
 USERNAME = os.environ.get('_username')
 PASSWORD = os.environ.get('_password')
 
+class Emoji(enum):
+    cycling = '🚴‍♀'
+    meditation = '🧘‍♀️'
+    strength = '🏋️‍♀️'
+    cardio = '🥊'
+    running = '🏃‍♀️'
+    walking = '🚶‍♀️'
 
 def _peloton_session(s):
 
@@ -39,7 +48,7 @@ def _peloton_session(s):
         user id, session id, etc
 
 	'''
-	payload = {'username_or_email': USERNAME, 'password': PASSWORD} 
+	payload = {'username_or_email': USERNAME, 'password': PASSWORD}
 	headers = {
         "Content-Type": "application/json",
     }
@@ -55,21 +64,21 @@ def _get_reservations(session, usr_id, headers):
 	'''
 	Using the session started and the user id retrieved, get
 	the reservations for this user - reservations = upcoming classes
-	on the user's schedule - send each reservation id and get the 
+	on the user's schedule - send each reservation id and get the
 	ride id
 
     Parameters
     ----------
-	session  
+	session
 		session started in _peloton_session
-	usr_id   
+	usr_id
 		user id retrieved when session started
-	headers  
+	headers
 		headers used for all calls asking for json content
 
 	'''
 	reservation_url = base_url + '/api/user/' + usr_id + '/reservations'
-	    
+
 	response = session.get(reservation_url, headers=headers)
 	reservations = response.json()
 
@@ -78,68 +87,81 @@ def _get_reservations(session, usr_id, headers):
 
 def _get_ride_id(session, reservation_id, headers):
 
-	'''
-	Using the reservation id, get the ride id for the specified reservations
-	use the ride id to retrieve ride details including instructor, description, etc
+    '''
+    Using the reservation id, get the ride id for the specified reservations
+    use the ride id to retrieve ride details including instructor, description, etc
 
     Parameters
     ----------
-	session 		
-		session started in _peloton_session
-	reservation_id  
-		passed from _get_reservations
-	headers 		
-		headers used for all calls asking for json
+    session
+        session started in _peloton_session
+    reservation_id
+        passed from _get_reservations
+    headers
+        headers used for all calls asking for json
 
 	'''
-	ride_url = base_url + '/api/peloton/' + reservation_id
 
-	response = session.get(ride_url, headers=headers)
-	rides = response.json()
+    ride_url = base_url + '/api/peloton/' + reservation_id
 
-	for ride in range(1):
-		_get_ride_details(session, rides['ride_id'], headers)
+    response = session.get(ride_url, headers=headers)
+    rides = response.json()
+
+    for ride in range(1):
+        _get_ride_details(session, rides['ride_id'], headers)
 
 
 def _get_ride_details(session, ride_id, headers):
-	'''
-	Retrieves ride details using the ride_id passed from _get_ride_id
-	in order to begin building calendar api response body properties
 
+    '''
+    Retrieves ride details using the ride_id passed from _get_ride_id
+    in order to begin building calendar api response body properties
 
     Parameters
     ----------
-	session 		
-		session started in _peloton_session
-	ride_id  
-		passed from _get_ride_id
-	headers 		
-		headers used for all calls asking for json
+    session
+        session started in _peloton_session
+	ride_id
+        passed from _get_ride_id
+	headers
+        headers used for all calls asking for json
+    '''
 
-	'''
-	ride_url = base_url + '/api/ride/' + ride_id + '/details'
+    ride_url = base_url + '/api/ride/' + ride_id + '/details'
+    response = session.get(ride_url, headers=headers)
+    rides = response.json()
 
-	response = session.get(ride_url, headers=headers)
-	rides = response.json()
-
-	# print(rides['ride']['duration'])
-	# print(rides['ride']['fitness_discipline'])
-    # workout_type = rides['ride']['fitness_discipline']
-	summary = rides['ride']['title'] + " " + rides['ride']['instructor']['name']
-	start_time = rides['ride']['scheduled_start_time']
-    _get_emoji(rides['ride']['fitness_discipline'])
+    discipline = rides['ride']['fitness_discipline']
+    duration = rides['ride']['duration']
+    emoji = _get_emoji(rides['ride']['fitness_discipline'])
+    summary = str(emoji) + ' ' + rides['ride']['title'] + ' ' + rides['ride']['instructor']['name']
+    start_time = rides['ride']['scheduled_start_time']
+    end_time = _get_end_time(start_time, duration)
 
 def _get_end_time(start_time, duration):
+    '''
+    '''
     print(start_time)
     print(duration)
 
 def _get_emoji(fitness_discipline):
-    print(fitness_discipline)
+    '''
+    '''
+
+    match fitness_discipline:
+        case 'cycling':
+            return Emoji.cycling.value
+        case 'cardio':
+            return Emoji.cardio.value
+        case 'meditation':
+            return Emoji.meditation.value
+        case _:
+            print("default")
 
 def main():
 	'''
 	main method which asks for a session to start and stores the user id required
-	for the next api call to get reservation ids 
+	for the next api call to get reservation ids
 
 	'''
 	s = requests.Session()
--- google_cal_event_creator.py
+++ google_cal_event_creator.py
@@ -17,7 +17,7 @@ def _calendar_api_call(summary, start_time, end_time, timezone):
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    
+
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
@@ -37,15 +37,15 @@ def _calendar_api_call(summary, start_time, end_time, timezone):
 
         # # Call the Calendar API
         # new_cal_event(service)
-       
+
         # events_result = service.events().list(calendarId='primary', timeMin=now,
-        #                                       maxResults=10, singleEvents=True,
-        #                                       orderBy='startTime').execute()
+        # maxResults=10, singleEvents=True,
+        # orderBy='startTime').execute()
         # events = events_result.get('items', [])
 
         # if not events:
-        #     print('No upcoming events found.')
-        #     return
+        # print('No upcoming events found.')
+        # return
 
         # # Prints the start and name of the next 10 events
         # for event in events:
@@ -59,10 +59,10 @@ def _calendar_api_call(summary, start_time, end_time, timezone):
 ## Research google api
 ## determine how build event for calendar event
 ## create function to pull duration of class and time of class to create end_time var
-## determine what part will go into the event name ++ summary 
+## determine what part will go into the event name ++ summary
 ###
 ## https://developers.google.com/calendar/api/v3/reference
-def _new_cal_event(service, summary, start_time, end_time, timezone): 
+def _new_cal_event(service, summary, start_time, end_time, timezone):
     # Title of the event.
     """
 
