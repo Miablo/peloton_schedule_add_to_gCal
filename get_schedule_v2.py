80a370e98687828a3c05f6d53556d455025f0ffa
Mio Diaz <themio@themioshow.com>
2022-10-08 19:27:01 -0400

    Google API added calendar events with emojis to calendar

--- get_schedule_v2.py
+++ get_schedule_v2.py
@@ -1,10 +1,11 @@
 import requests
 import peloton as Peloton
 import json
-
+import google_cal_event_creator as _cal_creator
 import datetime as _date_time
 import google_cal_event_creator as event_creator
 from dotenv import load_dotenv
+
 import os
 
 from enum import Enum as enum
@@ -21,6 +22,7 @@ load_dotenv()
 
 USERNAME = os.environ.get('_username')
 PASSWORD = os.environ.get('_password')
+CAL_ID = os.environ.get('_cal_id')
 
 class Emoji(enum):
     cycling = '🚴‍♀'
@@ -30,9 +32,9 @@ class Emoji(enum):
     running = '🏃‍♀️'
     walking = '🚶‍♀️'
 
-def _peloton_session(s):
 
-	'''
+def _peloton_session(s):
+    '''
 	Start peloton api session using username & password
 	save the user id returned in the json data for usage
 	later
@@ -46,22 +48,18 @@ def _peloton_session(s):
     -------
     dict
         user id, session id, etc
-
-	'''
-	payload = {'username_or_email': USERNAME, 'password': PASSWORD}
-	headers = {
-        "Content-Type": "application/json",
+    '''
+    payload = {'username_or_email': USERNAME, 'password': PASSWORD}
+    headers = {
+    "Content-Type": "application/json",
     }
-
-	response = s.post(base_url + '/auth/login', json=payload, headers=headers)
-	usr_info = response.json()
-
-	return usr_info
+    response = s.post(base_url + '/auth/login', json=payload, headers=headers)
+    usr_info = response.json()
+    return usr_info
 
 
 def _get_reservations(session, usr_id, headers):
-
-	'''
+    '''
 	Using the session started and the user id retrieved, get
 	the reservations for this user - reservations = upcoming classes
 	on the user's schedule - send each reservation id and get the
@@ -75,17 +73,12 @@ def _get_reservations(session, usr_id, headers):
 		user id retrieved when session started
 	headers
 		headers used for all calls asking for json content
-
-	'''
-	reservation_url = base_url + '/api/user/' + usr_id + '/reservations'
-
-	response = session.get(reservation_url, headers=headers)
-	reservations = response.json()
-
-	for reservation in reservations['data']:
-		_get_ride_id(session, reservation['id'], headers)
-
-def _get_ride_id(session, reservation_id, headers):
+    '''
+    reservation_url = base_url + '/api/user/' + usr_id + '/reservations'
+    response = session.get(reservation_url, headers=headers)
+    reservations = response.json()
+    for reservation in reservations['data']:
+        _get_ride_id(session, reservation['peloton_id'], headers)
 
     '''
     Using the reservation id, get the ride id for the specified reservations
@@ -99,55 +92,73 @@ def _get_ride_id(session, reservation_id, headers):
         passed from _get_reservations
     headers
         headers used for all calls asking for json
-
-	'''
-
+    '''
+def _get_ride_id(session, reservation_id, headers):
     ride_url = base_url + '/api/peloton/' + reservation_id
-
     response = session.get(ride_url, headers=headers)
     rides = response.json()
+    class_category = rides['live_class_category']
+    if class_category != "":
+        start_time = rides['scheduled_start_time']
+    else:
+        start_time = rides['pedaling_start_time']
 
-    for ride in range(1):
-        _get_ride_details(session, rides['ride_id'], headers)
-
-
-def _get_ride_details(session, ride_id, headers):
+    _get_ride_details(session, rides['ride_id'], headers, start_time)
 
     '''
     Retrieves ride details using the ride_id passed from _get_ride_id
     in order to begin building calendar api response body properties
-
     Parameters
     ----------
     session
         session started in _peloton_session
-	ride_id
+    ride_id
         passed from _get_ride_id
-	headers
+    headers
         headers used for all calls asking for json
     '''
-
+def _get_ride_details(session, ride_id, headers, start_time):
     ride_url = base_url + '/api/ride/' + ride_id + '/details'
     response = session.get(ride_url, headers=headers)
     rides = response.json()
-
     discipline = rides['ride']['fitness_discipline']
     duration = rides['ride']['duration']
     emoji = _get_emoji(rides['ride']['fitness_discipline'])
     summary = str(emoji) + ' ' + rides['ride']['title'] + ' ' + rides['ride']['instructor']['name']
-    start_time = rides['ride']['scheduled_start_time']
-    end_time = _get_end_time(start_time, duration)
-
-def _get_end_time(start_time, duration):
-    '''
-    '''
+    print(summary)
+    start_time = _date_time.datetime.fromtimestamp(start_time)
     print(start_time)
-    print(duration)
+    end_time = _get_end_time(start_time, duration)
+    new_format = "%Y-%m-%dT%H:%M:%S-04:00"
+    _cal_creator._calendar_api_call(summary, start_time.strftime(new_format), end_time.strftime(new_format), CAL_ID)
 
+    '''
+    Calculates end time of ride based on start time and duration
+
+    Parameters
+    ----------
+    start_timestamp
+        start time converted to datetime object
+    duration
+        duration of ride in seconds
+
+    Returns
+    ----------
+    datetime object
+        calculated end time of ride
+    '''
+def _get_end_time(start_timestamp, duration):
+    return start_timestamp + _date_time.timedelta(seconds=duration)
+
+    '''
+    Selects appropriate emoji for calendar event summary based on fitness
+    discipline of class
+    Parameters
+    ----------
+    fitness_discipline
+        string
+    '''
 def _get_emoji(fitness_discipline):
-    '''
-    '''
-
     match fitness_discipline:
         case 'cycling':
             return Emoji.cycling.value
@@ -155,25 +166,29 @@ def _get_emoji(fitness_discipline):
             return Emoji.cardio.value
         case 'meditation':
             return Emoji.meditation.value
+        case 'strength':
+            return Emoji.strength.value
+        case 'running':
+            return Emoji.running.value
+        case 'walking':
+            return Emoji.walking.value
         case _:
-            print("default")
+            print(" ")
 
+    '''
+    main method which asks for a session to start and stores the user id required
+    for the next api call to get reservation ids
+    '''
 def main():
-	'''
-	main method which asks for a session to start and stores the user id required
-	for the next api call to get reservation ids
+    s = requests.Session()
+    usr_info = _peloton_session(s)
+    usr_id = usr_info['user_id']
 
-	'''
-	s = requests.Session()
-
-	usr_info = _peloton_session(s)
-	usr_id = usr_info['user_id']
-
-	headers = {
+    headers = {
         "Content-Type": "application/json",
     }
 
-	_get_reservations(s, usr_id, headers)
+    _get_reservations(s, usr_id, headers)
 
 
 
